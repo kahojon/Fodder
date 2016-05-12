de039e96af1424e0a20cd4918cebae43b0a09fa7
Jonathan Leung <jonathan.leung@mail.mcgill.ca>
2016-05-12 16:06:37 -0400

    Initial Commit

---
+++ Fodder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fodder
+//
+//  Created by Jonathan Leung on 2016-05-12.
+//  Copyright © 2016 Housewives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
